556628bb23cd80581b9ccea3461fb78f62dd3489
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2020-02-03 15:52:50 +0100

    Add more entity reputation types and a comment for the ones still missing.

--- src/main/java/legends/model/EntityReputation.java
+++ src/main/java/legends/model/EntityReputation.java
@@ -2,6 +2,15 @@ package legends.model;
 
 import legends.xml.annotation.Xml;
 
+/**
+The entity and hf reputation types correspond to the wiki, but it is unknown           
+what their tags are...
+There's still grudge, brawler, murderer, comrade, member of respected group,
+member of hated group, friendly fighter, bully, brigand, loyal soldier, monster,
+flatterer, hunter, thief, and intruder.
+.47 seems to add something along the lines of preacher.
+*/
+
 public class EntityReputation {
 	@Xml("entity_id")
 	private int entityId;
@@ -12,6 +21,8 @@ public class EntityReputation {
 	@Xml("unsolved_murders")
 	private int unsolvedMurders;
 
+	@Xml("rep_hero")
+	private int repHero = -1;
 	@Xml("rep_trade_partner")
 	private int repTradePartner = -1;
 	@Xml("rep_enemy_fighter")
@@ -22,8 +33,14 @@ public class EntityReputation {
 	private int repPoet = -1;
 	@Xml("rep_bard")
 	private int repBard = -1;
+	@Xml("rep_dancer")
+	private int repDancer = -1;
 	@Xml("rep_storyteller")
 	private int repStoryteller = -1;
+	@Xml("rep_treasure_hunter")
+	private int repTreasureHunter = -1;
+	@Xml("rep_knowledge_preserver")
+	private int repKnowledgePreserver = -1;
 
 	public int getEntityId() {
 		return entityId;
@@ -55,6 +72,14 @@ public class EntityReputation {
 
 	public void setUnsolvedMurders(int unsolvedMurders) {
 		this.unsolvedMurders = unsolvedMurders;
+	}
+
+	public int getRepHero() {
+		return repHero;
+	}
+
+	public void setRepHero(int repHero) {
+		this.repHero = repHero;
 	}
 
 	public int getRepTradePartner() {
@@ -97,12 +122,36 @@ public class EntityReputation {
 		this.repBard = repBard;
 	}
 
+		public int getRepDancer() {
+		return repDancer;
+	}
+
+	public void setRepDancer(int repDancer) {
+		this.repDancer = repDancer;
+	}
+
 	public int getRepStoryteller() {
 		return repStoryteller;
 	}
 
 	public void setRepStoryteller(int repStoryteller) {
 		this.repStoryteller = repStoryteller;
+	}
+
+	public int getRepTreasureHunter() {
+		return repTreasureHunter;
+	}
+
+	public void setRepTreasureHunter(int repTreasureHunter) {
+		this.repTreasureHunter = repTreasureHunter;
+	}
+
+	public int getRepKnowledgePreserver() {
+		return repKnowledgePreserver;
+	}
+
+	public void setRepKnowledgePreserver(int repKnowledgePreserver) {
+		this.repKnowledgePreserver = repKnowledgePreserver;
 	}
 
 }
