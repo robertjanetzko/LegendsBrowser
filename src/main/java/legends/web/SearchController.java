8cf6e1405779b68b9e47b5c7fe6f8de3ca2b229f
Robert Janetzko <robert_janetzko@web.de>
2020-03-01 11:53:39 +0100

    limit search constant

--- src/main/java/legends/web/SearchController.java
+++ src/main/java/legends/web/SearchController.java
@@ -15,6 +15,8 @@ import legends.web.util.SearchResult;
 
 @Controller
 public class SearchController {
+	
+	private static final int SEARCH_LIMIT = 10;
 
 	@RequestMapping("/search")
 	public Template search(VelocityContext context) {
@@ -46,21 +48,21 @@ public class SearchController {
 
 		List<SearchResult> results = new ArrayList<>();
 
-		World.getEntities().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getEntities().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
-		World.getSites().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getSites().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
-		World.getStructures().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getStructures().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
-		World.getHistoricalFigures().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getHistoricalFigures().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
-		World.getIdentities().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getIdentities().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
-		World.getRegions().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getRegions().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
-		World.getArtifacts().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getArtifacts().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
-		World.getWorldConstructions().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(6)
+		World.getWorldConstructions().stream().filter(e -> e.getName().toLowerCase().contains(query)).limit(SEARCH_LIMIT)
 				.map(e -> new SearchResult(e.getName(), e.getURL())).forEach(results::add);
 
 		context.put("results", results);
